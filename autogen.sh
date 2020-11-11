036340455dde6f788037de73bdc6ecb16f17b47e
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-11-10 19:19:21 -0600

    Add autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if hash glibtoolize 2>/dev/null; then
+    glibtoolize --copy --force
+else 
+    libtoolize --copy --force
+fi
+aclocal
+autoconf
+autoheader --force
+automake --foreign --add-missing --copy
